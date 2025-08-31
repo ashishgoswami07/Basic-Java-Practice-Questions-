55b684364fa862322cc68090265a62672e4115ee
Ashish Goswami <ashishgoswami93746@gmail.com>
2025-08-31 17:17:26 +0530

    Pushing local Java practice questions

---
+++ Basic Print Sample.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("I" + "\n" + "Love" + "\n" + "CodeChef");
+    }
+}
+
+Your Output
+I
+Love
+CodeChef
