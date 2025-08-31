06b421a34706f21cf79ed64c54bf22e202e8a70c
Ashish Goswami <ashishgoswami93746@gmail.com>
2025-08-31 17:37:40 +0530

    Added for loop basic program

---
+++ for Loop Basic.java
@@ -0,0 +1,17 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		for(int i = 1; i<=5; i++) {
+		    int square = i * i;
+		    System.out.println(i + "-" + square);
+		}
+	}
+}
+
+Your Output
+1-1
+2-4
+3-9
+4-16
+5-25
