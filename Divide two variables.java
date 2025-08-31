9d7c3da19a8680e756293e4f5e846c0aebfbff87
Ashish Goswami <ashishgoswami93746@gmail.com>
2025-08-31 18:02:55 +0530

    Added Divide two variables program

---
+++ Divide two variables.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Write your code here 
+        int x = 20;
+        int y = 6;
+        int result = x/y;
+        System.out.println(result);
+    }
+}
+
+Your Output
+3
